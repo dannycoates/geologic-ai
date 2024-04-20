853aacc102ef0fe3862f4facac628dd9b6144fe4
Danny Coates <dannycoates@gmail.com>
2024-04-20 12:30:45 -0600

    reorder classes

--- js/nn.js
+++ js/nn.js
@@ -1,31 +1,229 @@
 /**
- * Sigmoid is the activation function used by the neurons.
- * Sigmoid produces an S-shaped curve between 0 and 1.
- * Negative inputs produce values close to 0, and
- * positive inputs produce values close to 1.
- *
- * Other activation functions like ReLU (Rectified Linear Unit)
- * may also be used, but sigmoid is simple and works well for
- * this example.
+ * A fully connected feedforward neural network.
  * 
- * @see {@link https://en.wikipedia.org/wiki/Sigmoid_function}
- *
- * @param {number} x - The input value.
- * @returns {number} - The sigmoid value.
+ * This type of neural network is made up of layers of neurons. Each layer gets
+ * its input from the previous layer and feeds its output to the next layer. The
+ * neurons in each layer take input from all the neurons in the previous layer.
+ * 
+ * The output of the last layer is the output of the network.
+ * 
+ * The network is trained using a supervised learning technique called backpropagation.
+ * Supervised means that the network is given the correct answer during training. It
+ * uses the correct answer to calculate the error of it's output and adjust the weights
+ * and biases of the neurons to reduce the error. The algorithm used to adjust the weights
+ * and biases is called stochastic gradient descent.
+ * 
+ * @see {@link https://en.wikipedia.org/wiki/Feedforward_neural_network}
+ * @see {@link https://en.wikipedia.org/wiki/Backpropagation}
+ * @see {@link https://en.wikipedia.org/wiki/Stochastic_gradient_descent}
  */
-function sigmoid(x) {
-  return 1 / (1 + Math.exp(-x));
+export class NeuralNetwork {
+  /**
+   * The layers of the neural network.
+   * @type {Layer[]}
+   */
+  layers = [];
+
+  /**
+   * Creates a new instance of the NeuralNetwork class.
+   * 
+   * @param {...number} layerSizes - The sizes of the layers in the neural network.
+   * @example
+   * // create a network with 3 layers: 784 input, 16 hidden, 11 output
+   * const network = new NeuralNetwork(784, 16, 11);
+   */
+  constructor(...layerSizes) {
+    // The input layer is implicit.
+    // It's size determines the number of inputs to the next layer.
+    for (let i = 1; i < layerSizes.length; i++) {
+      this.layers.push(new Layer(layerSizes[i], layerSizes[i - 1]));
+    }
+  }
+
+  /**
+   * The last layer in the neural network.
+   * @type {Layer}
+   */
+  get outputLayer() {
+    return this.layers[this.layers.length - 1];
+  }
+
+  /**
+   * Feeds forward the inputs through the neural network and calculates the outputs.
+   * 
+   * @param {number[]} inputs - The input values.
+   * @returns {number[]} - The output values of the neural network.
+   */
+  feedForward(inputs) {
+    for (const layer of this.layers) {
+      // the output of the current layer becomes the input of the next layer
+      inputs = layer.feedForward(inputs);
+    }
+    return inputs;
+  }
+
+  /**
+   * Backpropagates the error through the neural network.
+   * 
+   * @param {number[]} targetOutputs - The target output values from the training data.
+   * @returns {number} - The mean squared error of this iteration.
+   *                     This tells us how well the network is learning.
+   *                     The lower the error, the better the network is performing.
+   *                     @see {@link https://en.wikipedia.org/wiki/Mean_squared_error}
+   */
+  backpropagate(targetOutputs) {
+    let squaredErrorSum = 0;
+    // We process the layers in reverse order. The ouput layer
+    // uses the target outputs, while the hidden layers use the
+    // weights and deltas from the next layer.
+    for (let i = 0; i < targetOutputs.length; i++) {
+      const neuron = this.outputLayer.neurons[i];
+      const target = targetOutputs[i];
+      // Intuitively, the error of the output layer is the difference
+      // between the target output and the actual output.
+      // When the output is close to the target, the error is small.
+      const error = target - neuron.output;
+      neuron.backpropagate(error);
+      // Mean squared error is a simple way to measure the error.
+      // Mathematically, it's referred to as a loss or cost function.
+      // There are many other loss functions to choose from.
+      // Ours is a quadratic loss function.
+      // @see {@link https://en.wikipedia.org/wiki/Loss_function#Quadratic_loss_function}
+      squaredErrorSum += error * error;
+    }
+
+    for (let i = this.layers.length - 2; i >= 0; i--) {
+      this.layers[i].backpropagate(this.layers[i + 1]);
+    }
+    return squaredErrorSum / targetOutputs.length;
+  }
+
+  /**
+   * Updates the weights and biases of the neural network.
+   * 
+   * @param {number} learningRate - The learning rate.
+   */
+  updateWeights(learningRate) {
+    for (const layer of this.layers) {
+      layer.updateWeights(learningRate);
+    }
+  }
+
+  /**
+   * Trains the neural network using the given inputs and target outputs.
+   * 
+   * @param {import("./mnist").LabelledInputData[]} inputs - The input values.
+   * @param {number} [learningRate=1] - The higher the learning rate, the faster the network learns.
+   *                                However, if it's too high, the network may not converge.
+   * @param {number} [epochs=1] - The number of rounds of training to do over all the inputs.
+   *                              More epochs can improve accuracy, but can also lead to overfitting.
+   * @param {number} [batchSize=1] - The number of samples to process before updating the weights.
+   *                                 Larger batch sizes can speed up training, but may be less accurate.
+   */
+  train(inputs, learningRate = 1, epochs = 1, batchSize = 1) {
+    console.time("Training time");
+    const trainingData = inputs.map(({ input, label }) => ({
+      input,
+      // Transform the label into an array representing the desired output layer values.
+      output: Array.from({ length: this.outputLayer.size }, (_, i) =>
+        i === label ? 1 : 0
+      ),
+    }));
+    for (let i = 1; i <= epochs; i++) {
+      console.time(`Epoch ${i}/${epochs}`);
+      const epoch = trainingData
+        .concat(
+          noise(
+            trainingData[0].input.length,
+            trainingData[0].output.length,
+            trainingData.length / 10
+          )
+        )
+        .sort(() => Math.random() - 0.5);
+      let error = 0;
+      for (let j = 0; j < epoch.length; j += batchSize) {
+        const batch = epoch.slice(j, j + batchSize);
+        for (const { input, output } of batch) {
+          this.feedForward(input);
+          error += this.backpropagate(output);
+        }
+        this.updateWeights(learningRate);
+      }
+      console.timeEnd(`Epoch ${i}/${epochs}`);
+      console.log(`mean squared error: ${error / epoch.length}`);
+    }
+    console.timeEnd("Training time");
+  }
 }
 
 /**
- * Calculates the derivative of the sigmoid function for a given value.
- * This gives the slope of the curve at that point.
- *
- * @param {number} sigmoid - A sigmoid value (0-1) computed earlier.
- * @returns {number} - The sigmoid derivative value.
+ * A layer of neurons in a neural network.
  */
-function sigmoidDerivative(sigmoid) {
-  return sigmoid * (1 - sigmoid);
+class Layer {
+  /**
+   * The neurons in the layer.
+   * @type {Neuron[]}
+   */
+  neurons = [];
+
+  /**
+   * Creates a new instance of the Layer class.
+   * 
+   * @param {number} neuronCount - The number of neurons in the layer.
+   * @param {number} inputCount - The number of inputs per neuron.
+   */
+  constructor(neuronCount, inputCount) {
+    this.neurons = Array.from(
+      { length: neuronCount },
+      () => new Neuron(inputCount)
+    );
+  }
+
+  get size() {
+    return this.neurons.length;
+  }
+
+  /**
+   * Feeds forward the inputs through the layer and calculates the outputs.
+   * 
+   * @param {number[]} inputs - The input values.
+   * @returns {number[]} - The output values of the layer.
+   */
+  feedForward(inputs) {
+    const outputs = Array(this.neurons.length);
+    for (let i = 0; i < this.neurons.length; i++) {
+      outputs[i] = this.neurons[i].feedForward(inputs);
+    }
+    return outputs;
+  }
+
+  /**
+   * Backpropagates the error through the layer.
+   * 
+   * @param {Layer} nextLayer - The next layer in the neural network.
+   */
+  backpropagate(nextLayer) {
+    for (let i = 0; i < this.neurons.length; i++) {
+      // The error for this neuron is the sum of the error of all 
+      // the weights that took the output of this neuron as input.
+      let error = 0;
+      for (const neuron of nextLayer.neurons) {
+        error += neuron.errorForWeight(i);
+      }
+      this.neurons[i].backpropagate(error);
+    }
+  }
+
+  /**
+   * Updates the weights and biases of the neurons in the layer.
+   * 
+   * @param {number} learningRate - The learning rate.
+   */
+  updateWeights(learningRate) {
+    for (const neuron of this.neurons) {
+      neuron.updateWeights(learningRate);
+    }
+  }
 }
 
 /**
@@ -206,215 +404,33 @@ class Neuron {
 }
 
 /**
- * A layer of neurons in a neural network.
+ * Sigmoid is the activation function used by the neurons.
+ * Sigmoid produces an S-shaped curve between 0 and 1.
+ * Negative inputs produce values close to 0, and
+ * positive inputs produce values close to 1.
+ *
+ * Other activation functions like ReLU (Rectified Linear Unit)
+ * may also be used, but sigmoid is simple and works well for
+ * this example.
+ * 
+ * @see {@link https://en.wikipedia.org/wiki/Sigmoid_function}
+ *
+ * @param {number} x - The input value.
+ * @returns {number} - The sigmoid value.
  */
-class Layer {
-  /**
-   * The neurons in the layer.
-   * @type {Neuron[]}
-   */
-  neurons = [];
-
-  /**
-   * Creates a new instance of the Layer class.
-   * 
-   * @param {number} neuronCount - The number of neurons in the layer.
-   * @param {number} inputCount - The number of inputs per neuron.
-   */
-  constructor(neuronCount, inputCount) {
-    this.neurons = Array.from(
-      { length: neuronCount },
-      () => new Neuron(inputCount)
-    );
-  }
-
-  get size() {
-    return this.neurons.length;
-  }
-
-  /**
-   * Feeds forward the inputs through the layer and calculates the outputs.
-   * 
-   * @param {number[]} inputs - The input values.
-   * @returns {number[]} - The output values of the layer.
-   */
-  feedForward(inputs) {
-    const outputs = Array(this.neurons.length);
-    for (let i = 0; i < this.neurons.length; i++) {
-      outputs[i] = this.neurons[i].feedForward(inputs);
-    }
-    return outputs;
-  }
-
-  /**
-   * Backpropagates the error through the layer.
-   * 
-   * @param {Layer} nextLayer - The next layer in the neural network.
-   */
-  backpropagate(nextLayer) {
-    for (let i = 0; i < this.neurons.length; i++) {
-      // The error for this neuron is the sum of the error of all 
-      // the weights that took the output of this neuron as input.
-      let error = 0;
-      for (const neuron of nextLayer.neurons) {
-        error += neuron.errorForWeight(i);
-      }
-      this.neurons[i].backpropagate(error);
-    }
-  }
-
-  /**
-   * Updates the weights and biases of the neurons in the layer.
-   * 
-   * @param {number} learningRate - The learning rate.
-   */
-  updateWeights(learningRate) {
-    for (const neuron of this.neurons) {
-      neuron.updateWeights(learningRate);
-    }
-  }
+function sigmoid(x) {
+  return 1 / (1 + Math.exp(-x));
 }
 
 /**
- * A fully connected neural network.
+ * Calculates the derivative of the sigmoid function for a given value.
+ * This gives the slope of the curve at that point.
+ *
+ * @param {number} sigmoid - A sigmoid value (0-1) computed earlier.
+ * @returns {number} - The sigmoid derivative value.
  */
-export class NeuralNetwork {
-  /**
-   * The layers of the neural network.
-   * @type {Layer[]}
-   */
-  layers = [];
-
-  /**
-   * Creates a new instance of the NeuralNetwork class.
-   * 
-   * @param {...number} layerSizes - The sizes of the layers in the neural network.
-   * @example
-   * // create a network with 3 layers: 784 input, 16 hidden, 11 output
-   * const network = new NeuralNetwork(784, 16, 11);
-   */
-  constructor(...layerSizes) {
-    // The input layer is implicit.
-    // It's size determines the number of inputs to the next layer.
-    for (let i = 1; i < layerSizes.length; i++) {
-      this.layers.push(new Layer(layerSizes[i], layerSizes[i - 1]));
-    }
-  }
-
-  /**
-   * The last layer in the neural network.
-   * @type {Layer}
-   */
-  get outputLayer() {
-    return this.layers[this.layers.length - 1];
-  }
-
-  /**
-   * Feeds forward the inputs through the neural network and calculates the outputs.
-   * 
-   * @param {number[]} inputs - The input values.
-   * @returns {number[]} - The output values of the neural network.
-   */
-  feedForward(inputs) {
-    for (const layer of this.layers) {
-      // the output of the current layer becomes the input of the next layer
-      inputs = layer.feedForward(inputs);
-    }
-    return inputs;
-  }
-
-  /**
-   * Backpropagates the error through the neural network.
-   * 
-   * @param {number[]} targetOutputs - The target output values from the training data.
-   * @returns {number} - The mean squared error of this iteration.
-   *                     This tells us how well the network is learning.
-   *                     The lower the error, the better the network is performing.
-   *                     @see {@link https://en.wikipedia.org/wiki/Mean_squared_error}
-   */
-  backpropagate(targetOutputs) {
-    let squaredErrorSum = 0;
-    // We process the layers in reverse order. The ouput layer
-    // uses the target outputs, while the hidden layers use the
-    // weights and deltas from the next layer.
-    for (let i = 0; i < targetOutputs.length; i++) {
-      const neuron = this.outputLayer.neurons[i];
-      const target = targetOutputs[i];
-      // Intuitively, the error of the output layer is the difference
-      // between the target output and the actual output.
-      // When the output is close to the target, the error is small.
-      const error = target - neuron.output;
-      neuron.backpropagate(error);
-      // Mean squared error is a simple way to measure the error.
-      // Mathematically, it's referred to as a loss or cost function.
-      // There are many other loss functions to choose from.
-      // Ours is a quadratic loss function.
-      // @see {@link https://en.wikipedia.org/wiki/Loss_function#Quadratic_loss_function}
-      squaredErrorSum += error * error;
-    }
-
-    for (let i = this.layers.length - 2; i >= 0; i--) {
-      this.layers[i].backpropagate(this.layers[i + 1]);
-    }
-    return squaredErrorSum / targetOutputs.length;
-  }
-
-  /**
-   * Updates the weights and biases of the neural network.
-   * 
-   * @param {number} learningRate - The learning rate.
-   */
-  updateWeights(learningRate) {
-    for (const layer of this.layers) {
-      layer.updateWeights(learningRate);
-    }
-  }
-
-  /**
-   * Trains the neural network using the given inputs and target outputs.
-   * 
-   * @param {import("./mnist").LabelledInputData[]} inputs - The input values.
-   * @param {number} [learningRate=1] - The higher the learning rate, the faster the network learns.
-   *                                However, if it's too high, the network may not converge.
-   * @param {number} [epochs=1] - The number of rounds of training to do over all the inputs.
-   *                              More epochs can improve accuracy, but can also lead to overfitting.
-   * @param {number} [batchSize=1] - The number of samples to process before updating the weights.
-   *                                 Larger batch sizes can speed up training, but may be less accurate.
-   */
-  train(inputs, learningRate = 1, epochs = 1, batchSize = 1) {
-    console.time("Training time");
-    const trainingData = inputs.map(({ input, label }) => ({
-      input,
-      // Transform the label into an array representing the desired output layer values.
-      output: Array.from({ length: this.outputLayer.size }, (_, i) =>
-        i === label ? 1 : 0
-      ),
-    }));
-    for (let i = 1; i <= epochs; i++) {
-      console.time(`Epoch ${i}/${epochs}`);
-      const epoch = trainingData
-        .concat(
-          noise(
-            trainingData[0].input.length,
-            trainingData[0].output.length,
-            trainingData.length / 10
-          )
-        )
-        .sort(() => Math.random() - 0.5);
-      let error = 0;
-      for (let j = 0; j < epoch.length; j += batchSize) {
-        const batch = epoch.slice(j, j + batchSize);
-        for (const { input, output } of batch) {
-          this.feedForward(input);
-          error += this.backpropagate(output);
-        }
-        this.updateWeights(learningRate);
-      }
-      console.timeEnd(`Epoch ${i}/${epochs}`);
-      console.log(`mean squared error: ${error / epoch.length}`);
-    }
-    console.timeEnd("Training time");
-  }
+function sigmoidDerivative(sigmoid) {
+  return sigmoid * (1 - sigmoid);
 }
 
 /**
